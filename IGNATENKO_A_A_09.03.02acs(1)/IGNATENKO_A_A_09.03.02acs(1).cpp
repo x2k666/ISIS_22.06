be4a5d4f33f7503425d928fe1eee38b34083a3a3
x2k666 <killaura2018@mail.ru>
2023-06-22 16:37:22 +1000

    p.3 comm

--- IGNATENKO_A_A_09.03.02acs(1)/IGNATENKO_A_A_09.03.02acs(1).cpp
+++ IGNATENKO_A_A_09.03.02acs(1)/IGNATENKO_A_A_09.03.02acs(1).cpp
@@ -3,65 +3,117 @@
 #include <cstdlib>
 #include <omp.h>
 
-const int ARRAY_SIZE = 10e6; // Размер массива
+const int MATRIX_SIZE = 1000; // Размер матрицы
+
+void matrixMultiplicationSequential(float** A, float** B, float** C) {
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        for (int j = 0; j < MATRIX_SIZE; ++j) {
+            C[i][j] = 0.0f;
+            for (int k = 0; k < MATRIX_SIZE; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void matrixMultiplicationParallel(float** A, float** B, float** C) {
+#pragma omp parallel for
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        for (int j = 0; j < MATRIX_SIZE; ++j) {
+            C[i][j] = 0.0f;
+            for (int k = 0; k < MATRIX_SIZE; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
 
 int main() {
-    // Создание и заполнение массива одинарной точности
-    float* Asgl = new float[ARRAY_SIZE];
-    for (int i = 0; i < ARRAY_SIZE; ++i) {
-        Asgl[i] = static_cast<float>(rand()) / RAND_MAX; // Заполнение случайным числом в диапазоне [0, 1]
+    // Создание и заполнение матрицы A
+    float** A = new float* [MATRIX_SIZE];
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        A[i] = new float[MATRIX_SIZE];
+        for (int j = 0; j < MATRIX_SIZE; ++j) {
+            A[i][j] = static_cast<float>(rand()) / RAND_MAX; // Заполнение случайным числом в диапазоне [0, 1]
+        }
     }
 
-    // Создание и заполнение массива двойной точности
-    double* Adbl = new double[ARRAY_SIZE];
-    for (int i = 0; i < ARRAY_SIZE; ++i) {
-        Adbl[i] = static_cast<double>(rand()) / RAND_MAX; // Заполнение случайным числом в диапазоне [0, 1]
+    // Создание и заполнение матрицы B для одинарной точности
+    float** Bsgl = new float* [MATRIX_SIZE];
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        Bsgl[i] = new float[MATRIX_SIZE];
+        for (int j = 0; j < MATRIX_SIZE; ++j) {
+            Bsgl[i][j] = static_cast<float>(rand()) / RAND_MAX; // Заполнение случайным числом в диапазоне [0, 1]
+        }
+    }
+
+    // Создание и заполнение матрицы B для двойной точности
+    double** Bdbl = new double* [MATRIX_SIZE];
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        Bdbl[i] = new double[MATRIX_SIZE];
+        for (int j = 0; j < MATRIX_SIZE; ++j) {
+            Bdbl[i][j] = static_cast<double>(rand()) / RAND_MAX; // Заполнение случайным числом в диапазоне [0, 1]
+        }
+    }
+
+    // Создание матрицы C для одинарной точности
+    float** Csgl = new float* [MATRIX_SIZE];
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        Csgl[i] = new float[MATRIX_SIZE];
+    }
+
+    // Создание матрицы C для двойной точности
+    double** Cdbl = new double* [MATRIX_SIZE];
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        Cdbl[i] = new double[MATRIX_SIZE];
     }
 
     // Массивы для хранения времени выполнения для разных значений количества потоков
-    double elapsedTimesAsgl[5];
-    double elapsedTimesAdbl[5];
+    double elapsedTimesSeq[5];
+    double elapsedTimesPar[5];
 
-    // Измерение времени суммирования массивов для разных значений количества потоков
+    // Измерение времени умножения матриц для разных значений количества потоков
     int numThreads[] = { 2, 4, 8, 16, 32 };
     for (int i = 0; i < 5; ++i) {
         int numThread = numThreads[i];
         omp_set_num_threads(numThread); // Установка количества потоков
 
-        // Измерение времени суммирования массива одинарной точности
+        // Умножение матриц для одинарной точности (последовательно)
         clock_t start = clock();
-        float sumAsgl = 0.0f;
-#pragma omp parallel for reduction(+:sumAsgl)
-        for (int j = 0; j < ARRAY_SIZE; ++j) {
-            sumAsgl += Asgl[j];
-        }
+        matrixMultiplicationSequential(A, Bsgl, Csgl);
         clock_t end = clock();
-        double elapsedTimeAsgl = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+        double elapsedTimeSeq = static_cast<double>(end - start) / CLOCKS_PER_SEC;
 
-        // Измерение времени суммирования массива двойной точности
+        // Умножение матриц для одинарной точности (параллельно)
         start = clock();
-        double sumAdbl = 0.0;
-#pragma omp parallel for reduction(+:sumAdbl)
-        for (int j = 0; j < ARRAY_SIZE; ++j) {
-            sumAdbl += Adbl[j];
-        }
+        matrixMultiplicationParallel(A, Bsgl, Csgl);
         end = clock();
-        double elapsedTimeAdbl = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+        double elapsedTimePar = static_cast<double>(end - start) / CLOCKS_PER_SEC;
 
         // Запись времени выполнения в массивы
-        elapsedTimesAsgl[i] = elapsedTimeAsgl;
-        elapsedTimesAdbl[i] = elapsedTimeAdbl;
+        elapsedTimesSeq[i] = elapsedTimeSeq;
+        elapsedTimesPar[i] = elapsedTimePar;
     }
 
     // Вывод результатов в таблицу
-    std::cout << "Num Threads\tElapsed Time (single precision)\tElapsed Time (double precision)" << std::endl;
+    std::cout << "Num Threads\tElapsed Time (Sequential)\tElapsed Time (Parallel)" << std::endl;
     for (int i = 0; i < 5; ++i) {
-        std::cout << numThreads[i] << "\t\t" << elapsedTimesAsgl[i] << " seconds\t\t" << elapsedTimesAdbl[i] << " seconds" << std::endl;
+        std::cout << numThreads[i] << "\t\t" << elapsedTimesSeq[i] << " seconds\t\t" << elapsedTimesPar[i] << " seconds" << std::endl;
     }
 
     // Освобождение памяти
-    delete[] Asgl;
-    delete[] Adbl;
+    for (int i = 0; i < MATRIX_SIZE; ++i) {
+        delete[] A[i];
+        delete[] Bsgl[i];
+        delete[] Bdbl[i];
+        delete[] Csgl[i];
+        delete[] Cdbl[i];
+    }
+    delete[] A;
+    delete[] Bsgl;
+    delete[] Bdbl;
+    delete[] Csgl;
+    delete[] Cdbl;
 
     return 0;
 }
